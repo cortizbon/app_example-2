0eb566246b57b18763ea7dff8a4305df733ba4c4
Carlos Ortiz <67929766+cortizbon@users.noreply.github.com>
2023-07-24 14:42:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
+X = np.random.normal(0, 1, 1000)
+Y = np.random.normal(10, 1, 1000)
+
+st.title("Aplicación en clase")
+
+st.head("Add")
+         
+fig, ax = plt.subplots(1, 2, sharex=True)
+
+ax[0].hist(X, bins=50)
+ax[1].hist(Y, bins=50)
